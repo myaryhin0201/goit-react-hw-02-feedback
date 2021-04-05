ee469775a4cf2be9dab5474c11e11063cb270f76
myaryhin0201 <yarygin45@gmail.com>
2021-04-05 15:18:20 +0200

    Updates

--- static/js/main.f56f635d.chunk.js
+++ static/js/main.f56f635d.chunk.js
@@ -18,12 +18,13 @@
             width: '150px',
             padding: '10px 0',
             border: '0',
+            fontSize: '20px',
             borderRadius: '10px',
             '&:hover, &:focus': { backgroundColor: 'aqua', outline: 'none' },
             '&::first-letter': { textTransform: 'uppercase' },
           },
           good: { backgroundColor: 'lawngreen' },
-          bad: { backgroundColor: { backgroundColor: 'red' } },
+          bad: { backgroundColor: 'red' },
           neutral: { backgroundColor: 'orange' },
           FeedbackOptions__list: { display: 'flex', justifyContent: 'center' },
           FeedbackOptions__item: {
@@ -230,4 +231,4 @@
   },
   [[28, 1, 2]],
 ]);
-//# sourceMappingURL=main.4385f0e2.chunk.js.map
+//# sourceMappingURL=main.f56f635d.chunk.js.map
